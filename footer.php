43d51c2d5fbb57ce0da373b0b1b06b4ea8e9a356
Biplob14 <biploburrahaman@gmail.com>
2020-11-29 09:14:07 +0600

    firtst commit

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer>
+ <div class="container">
+   <div class="row text-center">
+   
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <ul class="list-inline">
+                <li> <a href="#"> <i class="fab fa-facebook-f fa-2x"> </i> </a> </li>
+                <li> <a href="#"> <i class="fab fa-dropbox fa-2x"> </i> </a> </li>
+                <li> <a href="#"> <i class="fab fa-github fa-2x"> </i> </a> </li>
+                <li> <a href="#"> <i class="fab fa-linkedin fa-2x"> </i> </a> </li>    
+            </ul>
+        </div>
+       
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <ul class="menu list-inline">
+                <li> <a href="#">Home</a> </li>
+                <li> <a href="#">About</a> </li>
+                <li> <a href="#">Blog</a> </li>
+                <li> <a href="#">Gallery </a> </li>
+                <li> <a href="#">Contact</a> </li>
+           </ul>
+           </div>
+       </div> 
+    </div>
+
+    <div class="search-text mx-auto"> 
+   <div class="container">
+        <div class="row text-center">
+            <div class="form">
+                <form id="search-form" class="form-search form-horizontal">
+                    <input type="text" class="input-search" placeholder="Search">
+                    <button type="submit" class="btn-search">Search</button>
+                </form>
+            </div>
+        </div>         
+   </div>     
+</div>
+</footer>
+
+<div class="copyright">
+ <div class="container">
+     <div class="row text-center">
+     	<p>Copyright © 2019 All rights reserved</p>
+     </div>
+    </div>
+</div>
+<!-- End -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- require header file  -->
+<?php require "header.php"; ?>
+</body>
+ <div class="main container">
+    <div class="row">
+        <div class="col-4">
+            <img src="img/slider2.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="col-8">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero beatae neque quia est ex quidem. Quis deserunt consequatur hic, 
+                magnam illo minima nostrum sed dolor fuga ipsum molestiae, et beatae.
+            </p>
+        </div>
+    </div>
+ </div>
+
+<!-- require footer file -->
+<?php require "footer.php"; 
+
+
